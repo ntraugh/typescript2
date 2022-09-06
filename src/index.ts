dc178e41c2d42fea9c1f2d6e401c01ca72a68bdd
Nate Traugh <n.traugh@yahoo.com>
2022-09-06 13:34:07 -0400

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,11 @@
-type ID = string    
-type PopularTag = string
-type MaybePopularTag = PopularTag | null
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
 
-// creating a global interface to use for our "users"
-interface UserInterface {
-    id?: ID;
-    name: string;
-    age?: number;
-    getMessage(): string;
-}
+import confetti from 'canvas-confetti';
 
-interface newUserInterface {
-    name: string,
-    age: string,
-
-}
-
-const popularTags: PopularTag[] = ["dragon", "cofee"]
-
-const dragonsTag: MaybePopularTag = "dragon"
-
-let username: string = "alex";
-// setting pageName to be either type of string OR number
-let pageName: string | number = "1"
-
-let errorMessage: string | null = null;
-
-let newUser: newUserInterface | null = null
-
-
-
-// specifying the type of parameters we pass in as well as the output we expect from the function
-
-const getFullName = (name: string, surname: string): string => {
-    return  name + " " + surname;
-}; 
-
-
-
-// typescript out of the box can tell what your object value types are
-const user: UserInterface =  {
-    name: "joey",
-    age: 30,
-    getMessage() {
-        return "Hello" + user.name
-    }
-}
-
-const user2: UserInterface = {
-    name: "Smith",
-    getMessage() {
-        return "Hello" + user.name
-    },
-}
-
+confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
+  resize: true,
+  useWorker: true,
+})({ particleCount: 200, spread: 200 });
