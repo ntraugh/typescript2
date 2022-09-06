a6a84f7725048ca98503eb80ae25ff293192e6eb
Nate Traugh <n.traugh@yahoo.com>
2022-09-06 13:53:32 -0400

    set up initial sumbit button for our form, query selected the elements and added the type of HTLM... to each query selection

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from "uuid"
 
-import confetti from 'canvas-confetti';
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+const list = document.querySelector<HTMLUListElement>("#list")
+const taskForm = document.querySelector<HTMLFormElement>("#new-task-form")
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+taskForm?.addEventListener("submit", e => {
+  e.preventDefault()
+
+  // adding ? after input to check for the null value
+  if(input?.value == "" || input?.value == null) return 
+
+  const task = {
+    id: uuidV4(),
+    title: input.value,
+    complete: false,
+    createdAt: new Date()
+  }
+  input.value
+})
